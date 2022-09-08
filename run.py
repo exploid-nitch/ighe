895bb9310e4c4e4dd1b064b324b5520cd8310b7b
exploid-nitch <105970605+exploid-nitch@users.noreply.github.com>
2022-09-08 22:41:35 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("exploid").ANJIM()
+       __import__("exploid").login_kamu()
    except Exception as e:
        exit(str(e))
