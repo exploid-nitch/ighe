07f30eeb7fd5bb16c87ad180ac9481b88d051842
exploid-nitch <105970605+exploid-nitch@users.noreply.github.com>
2022-09-08 22:37:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,7 @@
 import os
-try:os.system('mkdir OK')
-except:pass
-try:os.system('mkdir CP')
-except:pass
-try:os.system('touch .prox.txt')
-except:pass
-try:os.system('mkdir result')
-except:pass
 if __name__ == "__main__":
-        try:
-                __import__("exploid").lisensi()
-        except Exception as e:
-                exit(str(e))
+   try:
+       os.system("git pull")
+       __import__("exploid").ANJIM()
+   except Exception as e:
+       exit(str(e))
